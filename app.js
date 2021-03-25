db1d0c4a29e1fe6d44ebbffefe1d3702daf26a4c
IcEWaRRiOr <73989561+IcEWaRRiOr01@users.noreply.github.com>
2021-03-25 18:53:58 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var share = document.getElementById("share");
+
+var tooltip = document.getElementById("tooltip");
+
+document.getElementById("share").onclick = function () {
+    tooltip.classList.toggle("fade");
+}
